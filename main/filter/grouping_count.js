6d515816c0e47eb0deeb21c46b70a29c795177da
Song-Nan17 <songnan17@qq.com>
2018-12-23 15:30:27 +0800

    Rewrite grouping_count in es6.

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,17 +1,15 @@
 'use strict';
 
-function grouping_count(collection) {
-  return collection.reduce(countedElements, {});
-}
-
-function countedElements(allElements, ele) {
-  if (ele in allElements) {
-    allElements[ele]++;
-  }
-  else {
-    allElements[ele] = 1;
-  }
-  return allElements;
+let grouping_count = collection => {
+  return collection.reduce((allElements, ele) => {
+    if (ele in allElements) {
+      allElements[ele]++;
+    }
+    else {
+      allElements[ele] = 1;
+    }
+    return allElements;
+  }, {});
 }
 
 module.exports = grouping_count;
