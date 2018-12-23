119cb459bf0fb51b21be1d8bf928229de2614a0a
Song-Nan17 <songnan17@qq.com>
2018-12-23 16:24:05 +0800

    Rewrite collect_max_number in es6.

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -1,11 +1,5 @@
 'use strict';
 
-function collect_max_number(collection) {
-  return collection.reduce(maxNumber);
-}
-
-function maxNumber(max, current) {
-  return Math.max(max, current);
-}
+let collect_max_number = collection => collection.reduce((max, current) => Math.max(max, current));
 
 module.exports = collect_max_number;
