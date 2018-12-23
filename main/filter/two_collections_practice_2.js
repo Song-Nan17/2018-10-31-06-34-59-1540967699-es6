f7528f74b7257fe5883d868326dcc3cf6355ab9e
Song-Nan17 <songnan17@qq.com>
2018-12-23 15:35:28 +0800

    Rewrite choose_no_common_elements in es6.

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -1,9 +1,7 @@
 'use strict';
 
-function choose_no_common_elements(collection_a, collection_b) {
-  return collection_a.filter(function (element) {
-    return !collection_b.includes(element);
-  });
+let choose_no_common_elements = (collection_a, collection_b) => {
+  return collection_a.filter(element => !collection_b.includes(element));
 }
 
 module.exports = choose_no_common_elements;
