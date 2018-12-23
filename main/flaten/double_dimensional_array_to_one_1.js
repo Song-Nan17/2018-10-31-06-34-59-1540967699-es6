54c85747fc09da46296e4108e5df7ab4fc8cd63d
Song-Nan17 <songnan17@qq.com>
2018-12-23 15:46:33 +0800

    Format double_to_one.

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let double_to_one=collection=> {
-  return collection.reduce((accumulator, current)=>accumulator.concat(current), []);
+let double_to_one = collection => {
+  return collection.reduce((accumulator, current) => accumulator.concat(current), []);
 }
 
 module.exports = double_to_one;
