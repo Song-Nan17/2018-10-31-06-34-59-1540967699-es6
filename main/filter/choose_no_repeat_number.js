185a1743003bb9cf5f3204a53ec9a8763ec64e5a
Song-Nan17 <songnan17@qq.com>
2018-12-23 16:08:06 +0800

    Rewrite choose_no_repeat_number in es6.

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,11 +1,7 @@
 'use strict';
 
-function choose_no_repeat_number(collection) {
-  return collection.filter(noRepeat);
-}
-
-function noRepeat(element, index, array) {
-  return array.indexOf(element) === index;
+let choose_no_repeat_number = collection => {
+  return collection.filter((element, index, array) => array.indexOf(element) === index);
 }
 
 module.exports = choose_no_repeat_number;
