9abaf673dd3ed616bbc3f70a21ab92b6b5d6f38f
Song-Nan17 <songnan17@qq.com>
2018-12-23 15:54:14 +0800

    Rewrite map_to_even in es6.

--- main/map/map_to_even.js
+++ main/map/map_to_even.js
@@ -1,10 +1,4 @@
 'use strict';
-function map_to_even(collection) {
-  return collection.map(double);
-}
-
-function double(ele) {
-  return ele * 2;
-}
+let map_to_even = collection => collection.map(ele => ele * 2);
 
 module.exports = map_to_even;
