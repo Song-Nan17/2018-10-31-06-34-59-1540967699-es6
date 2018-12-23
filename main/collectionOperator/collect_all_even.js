76686c7557c48e24a77945a7b9f7691409c70107
Song-Nan17 <songnan17@qq.com>
2018-12-23 15:05:01 +0800

    Replace == by ===.

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -1,5 +1,5 @@
 'use strict';
 
-let collect_all_even = collection => collection.filter(element => element % 2 == 0);
+let collect_all_even = collection => collection.filter(element => element % 2 === 0);
 
 module.exports = collect_all_even;
