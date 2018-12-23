386c35f1c853fc00844e25cb948da3dc88cb52e9
Song-Nan17 <songnan17@qq.com>
2018-12-23 15:32:25 +0800

    Rewrite choose_common_elements in es6.

--- main/filter/two_collections_practice_1.js
+++ main/filter/two_collections_practice_1.js
@@ -1,9 +1,7 @@
 'use strict';
 
-function choose_common_elements(collection_a, collection_b) {
-  return collection_a.filter(function (element) {
-    return collection_b.includes(element);
-  });
+let choose_common_elements = (collection_a, collection_b) => {
+  return collection_a.filter(element => collection_b.includes(element));
 }
 
 module.exports = choose_common_elements;
