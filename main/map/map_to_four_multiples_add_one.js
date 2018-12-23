04aab2cf2c870b6169188ad4a171a2b6379a10dd
Song-Nan17 <songnan17@qq.com>
2018-12-23 15:58:19 +0800

    Use let.

--- main/map/map_to_four_multiples_add_one.js
+++ main/map/map_to_four_multiples_add_one.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var map_to_four_multiples_add_one = collection => collection.map(ele => ele * 4 + 1);
+let map_to_four_multiples_add_one = collection => collection.map(ele => ele * 4 + 1);
 
 module.exports = map_to_four_multiples_add_one;
