f0c4e7c2b73502138aa8481d3a8188571f65a00f
Song-Nan17 <songnan17@qq.com>
2018-12-23 16:03:54 +0800

    Rewrite number_map_to_word in es6.

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,11 +1,6 @@
 'use strict';
-var number_map_to_word = function (collection) {
-  return collection.map(numberToWord);
-};
-
-function numberToWord(number) {
-  let code = 97 + number - 1;
-  return String.fromCharCode(code);
+let number_map_to_word = collection => {
+  return collection.map(number => String.fromCharCode(97 + number - 1));
 }
 
 module.exports = number_map_to_word;
