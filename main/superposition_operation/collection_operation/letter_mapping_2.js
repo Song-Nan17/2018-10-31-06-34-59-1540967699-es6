766d18949c50ecd47adf45063c3ce6f00bde58c0
Song-Nan17 <songnan17@qq.com>
2018-12-24 17:01:03 +0800

    Rewrite letter_mapping_2 in es6.

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,20 +1,9 @@
 'use strict';
 
 function average_to_letter(collection) {
-  let code = Math.ceil(compute_average(collection));
-  return codeToLetter(code);
-}
-
-function compute_average(collection) {
-  return collection.reduce(sumArr) / collection.length;
-}
-
-function sumArr(accumulator, currentValue) {
-  return accumulator + currentValue;
-}
-
-function codeToLetter(code) {
-  return String.fromCharCode(97 + code - 1);
+  let sum = collection.reduce((acc, current) =>acc + current);
+  let average = Math.ceil(sum/collection.length);
+  return String.fromCharCode(97 + average - 1);
 }
 
 module.exports = average_to_letter;
