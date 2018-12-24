bb8608f26c239b8882a762d86bf55ef01f2e8989
Song-Nan17 <songnan17@qq.com>
2018-12-24 15:01:15 +0800

    Rewrite spilt_to_zero in es6.

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -1,12 +1,12 @@
 'use strict';
 
-function spilt_to_zero(number, interval) {
-  let splitArr = [number];
+let spilt_to_zero = (number, interval) => {
+  let split_arr = [number];
   do {
     number = (number * 10 - interval * 10) / 10;
-    splitArr.push(number);
+    split_arr.push(number);
   } while (number > 0);
-  return splitArr;
+  return split_arr;
 }
 
 module.exports = spilt_to_zero;
