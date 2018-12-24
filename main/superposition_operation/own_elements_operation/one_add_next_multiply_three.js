6bcd06ff28f513180a1c6e40aee8d70e33e0ffaf
Song-Nan17 <songnan17@qq.com>
2018-12-24 18:15:25 +0800

    Rewrite one_add_next_multiply_three in es6.

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,16 +1,15 @@
 'use strict';
-function one_add_next_multiply_three(collection) {
-  let computedArr = collection.map(addNextMultiplyThree);
-  computedArr.pop();
-  return computedArr;
-}
 
-function addNextMultiplyThree(currentValue, index, array) {
-  if (index < array.length - 1) {
-    let next = array[index + 1];
-    return (currentValue + next) * 3;
-  }
-  return currentValue;
+let one_add_next_multiply_three=collection=> {
+  let computed_arr = collection.map((current, index, array)=> {
+    if (index < array.length - 1) {
+      let next = array[index + 1];
+      return (current + next) * 3;
+    }
+    return current;
+  });
+  computed_arr.pop();
+  return computed_arr;
 }
 
 module.exports = one_add_next_multiply_three;
