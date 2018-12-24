fd51ded9b92742dcf7d84447704dee3efff0b4dd
Song-Nan17 <songnan17@qq.com>
2018-12-24 17:17:26 +0800

    Rewrite amount_even in es6.

--- main/superposition_operation/interval_operation/amount_even.js
+++ main/superposition_operation/interval_operation/amount_even.js
@@ -1,20 +1,8 @@
 'use strict';
 
-function amount_even(collection) {
-  let evenArr = collect_all_even(collection);
-  return evenArr.reduce(sumArr);
+let amount_even = collection => {
+  let even_arr = collection.filter(ele => ele % 2 === 0);
+  return even_arr.reduce((acc, current) => acc + current);
 }
-
-function collect_all_even(collection) {
-  return collection.filter(isEven);
-}
-function isEven(element) {
-  return element % 2 == 0;
-}
-
-function sumArr(accumulator, currentValue) {
-  return accumulator + currentValue;
-}
-
 
 module.exports = amount_even;
