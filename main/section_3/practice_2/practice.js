b586f6c055605ca56409d42eaa7a59419ce77097
Song-Nan17 <songnan17@qq.com>
2018-12-24 16:41:17 +0800

    Rewrite practice_2 of section_3 in es6.

--- main/section_3/practice_2/practice.js
+++ main/section_3/practice_2/practice.js
@@ -1,16 +1,10 @@
-function create_updated_collection(collection_a, object_b) {
-  let valueArr = object_b.value;
-  const findSameAndMinusOne = function (obj) {
-    if (valueArr.includes(obj.key)) {
-      obj.count -= minusOneEveryThree(obj.count);
+let create_updated_collection = (collection_a, object_b) => {
+  return collection_a.map(obj => {
+    if (object_b.value.includes(obj.key)) {
+      obj.count -= Math.floor(obj.count / 3);
     }
     return obj;
-  }
-  return collection_a.map(findSameAndMinusOne);
-}
-
-function minusOneEveryThree(num) {
-  return Math.floor(num / 3);
+  });
 }
 
 module.exports = create_updated_collection;
