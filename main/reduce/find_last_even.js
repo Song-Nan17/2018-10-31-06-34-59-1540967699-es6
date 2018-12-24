8ce3c752507f2ecaeb2a9ca7d265312f88435d06
Song-Nan17 <songnan17@qq.com>
2018-12-24 14:57:11 +0800

    Rewrite find_last_even in es6.

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -1,11 +1,7 @@
 'use strict';
 
-function find_last_even(collection) {
-  return collection.reverse().find(even);
-}
-
-function even(num) {
-  return num % 2 == 0;
+let find_last_even = collection => {
+  return collection.reverse().find(num => num % 2 == 0);
 }
 
 module.exports = find_last_even;
