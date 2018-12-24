8ad0f80a1cb922d0f9d10e59d68ea320efea780d
Song-Nan17 <songnan17@qq.com>
2018-12-24 17:11:02 +0800

    Rewrite hybrid_operation_to_uneven in es6.

--- main/superposition_operation/collection_operation/practice_2.js
+++ main/superposition_operation/collection_operation/practice_2.js
@@ -1,20 +1,8 @@
 'use strict';
 
-function hybrid_operation_to_uneven(collection) {
-  let oddArr = collect_all_odd(collection);
-  return oddArr.map(triplePlusTwo);
-}
-
-function collect_all_odd(collection) {
-  return collection.filter(isOdd);
-}
-
-function isOdd(element) {
-  return element % 2 !== 0;
-}
-
-function triplePlusTwo(num) {
-  return num * 3 + 2;
+let hybrid_operation_to_uneven = collection => {
+  let odd_arr = collection.filter(ele => ele % 2 !== 0);
+  return odd_arr.map(ele => ele * 3 + 2);
 }
 
 module.exports = hybrid_operation_to_uneven;
