3f1dd460def3e24730b143add55ad2b09680600f
Song-Nan17 <songnan17@qq.com>
2018-12-24 17:34:53 +0800

    Rewrite calculate_median in es6.

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,22 +1,15 @@
 'use strict';
-var calculate_median = function (collection) {
-  let evenSubArr = collectNumEvenEle(collection);
-  return calculateMedian(evenSubArr);
-};
 
-function collectNumEvenEle(collection) {
-  return collection.filter(isNumEven);
-}
-function isNumEven(element, index) {
-  if ((index + 1) % 2 == 0) {
-    return element;
-  }
+let calculate_median = collection => {
+  let even_sub_arr = collection.filter((element, index) => (index + 1) % 2 === 0);
+  return compute_median(even_sub_arr);
 }
 
-function calculateMedian(collection) {
-  let medianLeftSub = Math.floor((collection.length - 1) / 2);
-  let medianRightSub = Math.ceil((collection.length - 1) / 2);
-  let doubleMedian = collection[medianLeftSub] + collection[medianRightSub];
-  return doubleMedian / 2;
+let compute_median = collection => {
+  let median_left_sub = Math.floor((collection.length - 1) / 2);
+  let median_right_sub = Math.ceil((collection.length - 1) / 2);
+  let double_median = collection[median_left_sub] + collection[median_right_sub];
+  return double_median / 2;
 }
+
 module.exports = calculate_median;
