d81161a3be8609e1c9b716976bb9383544326b0a
Song-Nan17 <songnan17@qq.com>
2018-12-24 15:09:57 +0800

    Rewrite practice_2 of section_1 in es6.

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,21 +1,6 @@
 function collect_same_elements(collection_a, collection_b) {
-  const oneDimensionArr = toOneDimension(collection_b);
-  return collectSame(collection_a, oneDimensionArr);
-}
-
-function toOneDimension(collection) {
-  return collection.reduce(toConcatArr, []);
-}
-
-function toConcatArr(accumulator, currentValue) {
-  return accumulator.concat(currentValue);
-}
-
-function collectSame(collection_a, collection_b) {
-  const isInclude = function (element) {
-    return collection_b.includes(element);
-  }
-  return collection_a.filter(isInclude);
+  let one_dimension = collection_b.reduce((acc, current) => acc.concat(current), []);
+  return collection_a.filter(ele => one_dimension.includes(ele));
 }
 
 module.exports = collect_same_elements;
