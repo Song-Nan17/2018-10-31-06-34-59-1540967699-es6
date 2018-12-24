03cc6f4366a6cca2de3fd45ce2a03060db085d50
Song-Nan17 <songnan17@qq.com>
2018-12-24 18:09:31 +0800

    Rewrite even_asc_odd_desc in es6.

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,43 +1,11 @@
 'use strict';
 
-var even_asc_odd_desc = function (collection) {
-  let evenArr = choose_even(collection);
-  let oddArr = choose_odd(collection);
-  evenArr = rank_asc(evenArr);
-  oddArr = rank_desc(oddArr);
-  return evenArr.concat(oddArr);
-};
-
-function choose_even(collection) {
-  return collection.filter(isEven);
-}
-
-function isEven(element) {
-  return element % 2 == 0;
-}
-
-function choose_odd(collection) {
-  return collection.filter(isOdd);
-}
-
-function isOdd(element) {
-  return element % 2 !== 0;
-}
-
-function rank_asc(collection) {
-  return collection.sort(rankAsc);
-};
-
-function rankAsc(a, b) {
-  return a - b;
-}
-
-function rank_desc(collection) {
-  return collection.sort(rankDesc);
-};
-
-function rankDesc(a, b) {
-  return b - a;
+let even_asc_odd_desc = collection => {
+  let even_arr = collection.filter(ele => ele % 2 === 0);
+  let odd_arr = collection.filter(ele => ele % 2 !== 0);
+  even_arr = even_arr.sort((a, b) => a - b);
+  odd_arr = odd_arr.sort((a, b) => b - a);
+  return even_arr.concat(odd_arr);
 }
 
 module.exports = even_asc_odd_desc;
